0069bc8aab1c94133b10dfbb02894fc5d7f3e61c
Juan Camilo García <cuevas53verde@gmail.com>
2026-01-29 12:01:42 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/tasklists/MainActivity.kt
+++ app/src/main/java/com/example/tasklists/MainActivity.kt
@@ -27,32 +27,24 @@ class MainActivity : AppCompatActivity() {
         val botonAnadir = findViewById<Button>(R.id.btn_anadir)
         val vistaLista = findViewById<ListView>(R.id.lv_lista_tareas)
 
-        // 2. Datos y Adaptador
         val listaTareas = ArrayList<String>()
 
-        // ADAPTADOR: (Contexto, Diseño de fila, DATOS)
         val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaTareas)
 
         // 3. ¡VITAL! Conectar la vista con el adaptador
-        // Si no haces esto, la lista saldrá vacía siempre.
         vistaLista.adapter = adaptador
 
         // 4. El Click
         botonAnadir.setOnClickListener {
             val texto = inputTarea.text.toString()
 
-            // VALIDACIÓN: En vez de != null (que siempre es true), usa isNotEmpty()
             if (texto.isNotEmpty()) {
                 listaTareas.add(texto)
-
-                // AVISAR AL CAMARERO: Refrescar la lista
+                
                 adaptador.notifyDataSetChanged()
 
-                // TRUCO PRO: Borrar el texto del input para escribir otra tarea rápido
                 inputTarea.text.clear()
             } else {
-                // EL TOAST (Ventana emergente)
-                // Toast.makeText(contexto, texto, duracion).show()
                 android.widget.Toast.makeText(this, "Escribe algo primero", android.widget.Toast.LENGTH_SHORT).show()
             }
         }
